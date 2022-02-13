203849fb0c4d0a0381d3e3892a76a52acc8470c9
Krzysiek <nexori>
2022-02-13 13:52:57 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,74 +4,72 @@
 #include <string.h> 
 #include <VL53L0X.h> 
 #include <Stepper.h>
-/**
-* Notes:
-* Each instruction cycle takes 250ns                                       
-*/
-// Pin definition
-#define A	4
-#define AN	5
-#define B	6
-#define BN	7
-#define TransOptToLED	8
-#define TransOptFromOptocopuler	9
-#define motorEnable 10
 
-VL53L0X sensor;
-motor m(A,AN,B,BN,400);
-Stepper mot(400, 4,5,6,7);
+// Pin definition
+#define A	6
+#define AN	7
+#define B	8
+#define BN	9
+#define TransOptToLED	3
+#define TransOptFromOptocopuler	2
+#define motorEnable 4
+
+
+
+VL53L0X sensor; 
+Stepper mot(200, A,AN,B,BN);
+
 int setSpeed;
 int stepSkip;
 int timingBudget;
-// Main() function / initialization
+
 void setup() {
-	setSpeed = 60;
-	stepSkip = 8;
-	timingBudget = 20; 
-
-  	Serial.begin(57600); //Start serial communication 
+	//Start serial communication  
+	Serial.begin(57600); 
 	Serial.setTimeout(50);
-	Wire.begin(); 
-	mot.setSpeed(setSpeed);
-	
-	//while (!Serial);		//Wait until USB serial communication is established
-	
-	delay(100);
-	
 
-	//sensor.init(); 				Serial.println("BL0"); 
+	delay(5000);
 	
-	//while (!sensor.init())
-	//{
-	//	Serial.println("Failed to detect and initialize sensor!");
-	//	sensor.init();
-	//	delay(1000);
-	//}
+	//Set global variables
+	setSpeed = 6;  
+	stepSkip = 1; 
+	timingBudget = 20; 
+	mot.setSpeed(setSpeed); 				Serial.println("BT: Parameters set"); 
 
-	sensor.setTimeout(500);				Serial.println("BL1"); 
-	sensor.setSignalRateLimit(0.25); 	Serial.println("BL2"); 
+	//Initialize sensor
+	Wire.begin();  							Serial.println("BT: I2C Started"); 
+	delay(1000);
+	sensor.init(); 							Serial.println("BT: Init sensor"); 
+	while (!sensor.init())
+	{
+		Serial.println("BT: Failed to detect and initialize sensor");
+		sensor.init();
+		delay(1000);
+	}
 
-	sensor.setMeasurementTimingBudget(timingBudget*1000);  Serial.println("BL3");  //Set measurement time budget to 20ms
-	sensor.startContinuous(20); 		Serial.println("BL4"); 
+	//Set sensor parameters
+	sensor.setTimeout(500);				
+	sensor.setSignalRateLimit(0.1); 	
+	sensor.setMeasurementTimingBudget(timingBudget*1000);  //Set measurement time budget to 20ms
+	sensor.startContinuous(20); 		Serial.println("BT: Sensor parameters set"); 
 
-	pinMode(TransOptFromOptocopuler, INPUT); Serial.println("BL5");
-	pinMode(TransOptToLED, OUTPUT);		Serial.println("BL6");
-	pinMode(motorEnable, OUTPUT);		Serial.println("BL7");
+	//Set DDR registers
+	pinMode(TransOptFromOptocopuler, INPUT); 	
+	pinMode(TransOptToLED, OUTPUT);				
+	pinMode(motorEnable, OUTPUT);		Serial.println("BT: DDR set"); 
 
-	pinMode(13, OUTPUT); 				Serial.println("BL8");
-	digitalWrite(motorEnable,HIGH);		Serial.println("BL9"); 
-	
-	delay(100); 						Serial.println("BL10");
-	 
-	
 	//Reset position
-	digitalWrite(TransOptToLED,HIGH);	Serial.println("BL12");	
-	//while (digitalRead(TransOptFromOptocopuler)==0)
-	//{
-	//	 mot.step(-1); Serial.println(digitalRead(TransOptFromOptocopuler)); delay(100);
-	//} 
-	digitalWrite(TransOptToLED,LOW); 	Serial.println("BL13");
-	loop();
+	digitalWrite(motorEnable,HIGH);		Serial.println("BL: Position reset");	
+	digitalWrite(TransOptToLED,HIGH);	
+	delay(1000);
+	while (digitalRead(TransOptFromOptocopuler)==0)
+	{
+		 mot.step(-1); Serial.println(digitalRead(TransOptFromOptocopuler)); delayMicroseconds(1250); Serial.println("BL18");
+	} 
+	mot.step_number = 0;
+	digitalWrite(TransOptToLED,LOW); 	
+	digitalWrite(motorEnable,LOW);		Serial.println("BL: Done");
+	loop();	
 }
  
 // Continous loop inside main() function
@@ -79,9 +77,9 @@ void loop()
 {
 	//Check for new speed settings:
 	if (Serial.peek() == '@' || Serial.peek() == '%' || Serial.peek() == '&'){
-		char cmd = Serial.peek();
-		String rx = Serial.readStringUntil('\n');
-		rx.remove(0,1);
+		char cmd = Serial.peek(); 
+		String rx = Serial.readStringUntil('\n'); 
+		rx.remove(0,1); 
 		switch(cmd)
 		{
 		case '@': // Set speed  
@@ -91,29 +89,53 @@ void loop()
 		case '%': // Set step skip size
 			stepSkip = rx.toInt(); 
 			break;
-		case '&': // Set timing budget 
-			sensor.stopContinuous();
-			timingBudget = rx.toInt(); 
-			sensor.setMeasurementTimingBudget(timingBudget*1000);
-			sensor.startContinuous();
+		case '&': // Set timing budget  
+			sensor.stopContinuous(); 
+			timingBudget = rx.toInt();
+			sensor.setMeasurementTimingBudget(timingBudget*1000); 
+			sensor.startContinuous(); 
 			break;
 		}
-		delay(3000); Serial.println("SETTINGS CHANGED");
+		delay(3000); 
 	}
-
-	//Serial.println(Serial.availableForWrite());
-	//If TX buffer is full, halt 
-	if (Serial.availableForWrite())
+	// Get the measurement 
+	
+	delay(timingBudget);
+	uint16_t meas = sensor.readRangeContinuousMillimeters(); 
+	
+	//Check for timeout error (0xFFFF) and reset device
+	while (meas == uint16_t(0xFFFF))
 	{
-	int now = millis();
-	int meas = sensor.readRangeContinuousMillimeters();
-	char buffer[100];
-	mot.step(stepSkip);
-
-	sprintf(buffer,"$%3d,%4d;%4d,%4d,%3d;%3d",mot.step_number, meas, setSpeed, stepSkip, timingBudget,int(millis()-now)); 
-	Serial.println(buffer);  
-
-	//Serial.print(digitalRead(A)); Serial.print(digitalRead(AN)); Serial.print(digitalRead(B)); Serial.print(digitalRead(BN));    
-	//Serial.print(digitalRead(TransOptToLED)); Serial.print(digitalRead(TransOptFromOptocopuler)); Serial.println(digitalRead(motorEnable)); 
+		sensor.stopContinuous(); Serial.println("13");
+		sensor.init();
+		sensor.setTimeout(500);				
+		sensor.setSignalRateLimit(0.25); 	
+		sensor.setMeasurementTimingBudget(timingBudget*1000);  
+		sensor.startContinuous();
+		delay(100);
+		meas = sensor.readRangeContinuousMillimeters();
 	}
+	//If TX buffer is full, halt 
+	if (Serial.availableForWrite()){
+	digitalWrite(motorEnable,HIGH);
+	int now = millis(); 
+	char buffer[50]; 
+	mot.step(stepSkip); 
+
+	//TX measurement data and parameters
+	sprintf(buffer,"$%4d,%4d;%4d,%4d,%3d;%3d ",
+					mot.step_number, 	// Motor shaft position
+					meas, 				// Sensor measurement results
+					setSpeed, 			// Motor speed
+					stepSkip, 			// Motor step skip
+					timingBudget,		// Sensor timing budget
+					int(millis()-now)); // Loop time
+	Serial.print(buffer);Serial.println("19");
+	Serial.print(digitalRead(TransOptToLED));  
+	Serial.println(digitalRead(TransOptFromOptocopuler));   Serial.println("20");
+	}
+	//If not transmitting, stop the motor
+	else digitalWrite(motorEnable,LOW);
+	
+	
 }
